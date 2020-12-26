0d43b9a544a849ed516b8cc0298c50103856fee5
karan_singhasane <karan.singhasane@radicali.io>
2020-12-26 23:00:27 +0530

    add react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css'
+import Todos from "./components/Todos.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "title 1",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "title 2",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "title 3",
+        completed: false,
+      }
+    ]
+  }
+
+  render() {
+    console.log(this.state.todos)
+    return (
+      <Todos />
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Todos extends React.Component {
+  render() {
+    return (
+        <h1>Todos Component</h1>
+    )
+  }
+}
+
+export default Todos
